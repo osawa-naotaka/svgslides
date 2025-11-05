4cd07e36085685efe66b62ddf1733b823131a8b7
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2025-11-06 00:16:51 +0900

    add vite electron dev environment.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "node develop environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        electron_38
+        bun
+      ];
+      shellHook = ''
+        export ELECTRON_OVERRIDE_DIST_PATH=${pkgs.electron_38}/bin
+      '';
+    };
+  };
+}
