dcd0a82a6996d2d2a4f9848c249ac19f3ec277e8
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2025-11-06 23:54:13 +0900

    fix for lint.

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,10 @@
-import { app, BrowserWindow } from 'electron';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { cwd } from 'node:process';
+import path from "node:path";
+import { cwd } from "node:process";
+import { fileURLToPath } from "node:url";
+import { app, BrowserWindow } from "electron";
 
 // const require = createRequire(import.meta.url)
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // The built directory structure
 //
@@ -15,50 +15,50 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // │ │ ├── main.js
 // │ │ └── preload.mjs
 // │
-process.env.APP_ROOT = path.join(__dirname, '..')
+process.env.APP_ROOT = path.join(__dirname, "..");
 
 // 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
-export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+export const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL;
 
-let win: BrowserWindow | null
+let win: BrowserWindow | null;
 
 function createWindow() {
-  win = new BrowserWindow({
-    icon: path.join(cwd(), "public", 'gear-round-svgrepo-com.svg'),
-    webPreferences: {
-      preload: path.join(cwd(), "dist-electron", "preload.mjs"),
-    },
-  })
+    win = new BrowserWindow({
+        icon: path.join(cwd(), "public", "gear-round-svgrepo-com.svg"),
+        webPreferences: {
+            preload: path.join(cwd(), "dist-electron", "preload.mjs"),
+        },
+    });
 
-  // Test active push message to Renderer-process.
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', (new Date).toLocaleString())
-  })
+    // Test active push message to Renderer-process.
+    win.webContents.on("did-finish-load", () => {
+        win?.webContents.send("main-process-message", new Date().toLocaleString());
+    });
 
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL)
-  } else {
-    // win.loadFile('dist/index.html')
-    win.loadFile(path.join(cwd(), "dist", 'index.html'))
-  }
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL);
+    } else {
+        // win.loadFile('dist/index.html')
+        win.loadFile(path.join(cwd(), "dist", "index.html"));
+    }
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-    win = null
-  }
-})
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+        win = null;
+    }
+});
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
+app.on("activate", () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
