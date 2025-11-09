63cb7741f83e3a75c254d13547d4b2ba1d95f9b3
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-11-09 11:26:32 +0900

    use react router.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router";
+import App from "./App.tsx";
 
 const root = document.getElementById("root");
 if (root === null) throw new Error("main.tsx: root element not found.");
 
 ReactDOM.createRoot(root).render(
-    <React.StrictMode>
+    <BrowserRouter>
         <App />
-    </React.StrictMode>,
+    </BrowserRouter>,
 );
+
